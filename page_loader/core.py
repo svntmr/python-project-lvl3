0354d2fe11c1aa7bc64e6189dcb0de1e086a4ca6
Savin Timur <mr.svntmr@gmail.com>
2022-04-02 22:32:59 +0300

    step 2: implement assets downloading feature
    - add code docstrings
    - add test for `page_loader.py` script
    - bump project version to 0.2.0
    - project author update
    - pre-commit config update to ignore .html files
    - add .prettierignore to ignore html files

--- page_loader/core.py
+++ page_loader/core.py
@@ -1,10 +1,154 @@
+from copy import copy
+from pathlib import Path
+from typing import Dict, List, Union
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag
 from page_loader.comm import get_page_content
-from page_loader.file_operations import store_page_content
+from page_loader.file_operations import (
+    generate_file_name_from_src,
+    generate_file_name_prefix_from_page_url,
+    save_assets,
+    save_file,
+)
 
 
-def download(page_url: str, output: str) -> str:
+def download(page_url: str, output: Path) -> str:
+    """
+    Downloads and stores page content with assets into provided folder
+    then returns path to saved page
+
+    :param page_url: page url to download
+    :type page_url: str
+    :param output: folder to save page content
+    :type output: Path
+    :return: path to saved page
+    :rtype: str
+    :raises RuntimeError: if output folder doesn't exist
+    """
     page_content = get_page_content(page_url)
 
-    file_path = store_page_content(page_url, page_content, output)
+    file_path = process_page_content(page_url, page_content, output)
 
     return file_path
+
+
+def process_page_content(page_url: str, content: str, folder: Path) -> str:
+    """
+    Processes page content and downloads assets into provided folder
+    then returns path to saved page
+
+    :param page_url: page url
+    :type page_url: str
+    :param content: page content
+    :type content: str
+    :param folder: folder to save page contents
+    :type folder: Path
+    :return: path to saved page
+    :rtype: str
+    :raises RuntimeError: if output folder doesn't exist
+    """
+    soup = BeautifulSoup(content, features="html.parser")
+    file_name_prefix = generate_file_name_prefix_from_page_url(page_url)
+    # parse and download page assets
+    assets = get_page_assets(soup)
+    # save assets using base name
+    updated_assets = process_assets(
+        assets=assets,
+        file_name_prefix=file_name_prefix,
+        page_url=page_url,
+        folder=folder,
+    )
+    # update page code with new assets
+    updated_content = update_page_assets(soup, updated_assets)
+    # save page new content
+    file_name = f"{file_name_prefix}.html"
+    filepath = save_file(updated_content, file_name, folder)
+
+    return str(filepath.resolve())
+
+
+def get_page_assets(soup: BeautifulSoup) -> List[Tag]:
+    """
+    Extracts assets from the given page
+
+    :param soup: page soup
+    :type soup: BeautifulSoup
+    :return: list of assets
+    :rtype: List[Tag]
+    """
+    images = soup.findAll("img")
+    return list(images)
+
+
+def process_assets(
+    assets: List[Tag], page_url: str, file_name_prefix: str, folder: Union[str, Path]
+) -> Dict[Tag, Tag]:
+    """
+    Creates the folder for assets storing, downloads and saves asset contents,
+    updates asset tags with new src and returns mapping with old assets
+    connected to updated assets
+
+    :param assets: list of asset tags
+    :type assets: List[Tag]
+    :param page_url: page url
+    :type page_url: str
+    :param file_name_prefix: prefix for the file names
+    :type file_name_prefix: str
+    :param folder: folder to save page contents
+    :type folder: Path
+    :return: mapping with old assets connected to updated assets
+    :rtype: Dict[Tag, Tag]
+    :raises RuntimeError: if output folder doesn't exist
+    """
+    if not assets:
+        return {}
+    # create folder
+    assets_folder = file_name_prefix + "_files"
+    assets_folder_path = Path(folder).joinpath(assets_folder)
+    assets_folder_path.mkdir(parents=True, exist_ok=True)
+
+    # download assets
+    assets_content = {
+        asset: get_page_content(page_url + asset.attrs["src"]) for asset in assets
+    }
+
+    new_assets_path_content = {}
+    updated_assets = {}
+    for asset in assets:
+        new_asset_file_name = generate_file_name_from_src(
+            file_name_prefix, asset.attrs["src"]
+        )
+        new_asset_path = assets_folder_path.joinpath(new_asset_file_name)
+        # assign new path to old content
+        new_assets_path_content[new_asset_path] = assets_content[asset]
+
+        # update asset with new src
+        asset_copy = copy(asset)
+        asset_copy.attrs["src"] = f"{assets_folder}/{new_asset_file_name}"
+        # connect old asset with new asset
+        updated_assets[asset] = asset_copy
+    # save assets
+    save_assets(new_assets_path_content)
+    # return assets with new names back
+    return updated_assets
+
+
+def update_page_assets(soup: BeautifulSoup, assets: Dict[Tag, Tag]) -> str:
+    """
+    Replaces old assets with new assets with updated src attribute
+
+    :param soup: page soup
+    :type soup: BeautifulSoup
+    :param assets: mapping with old assets connected to updated assets
+    :type assets: Dict[Tag, Tag]
+    :return: updated page content
+    :rtype: str
+    """
+    soup_copy = copy(soup)
+    for original_asset, updated_asset in assets.items():
+        original_asset_in_soup = soup_copy.find(
+            original_asset.name, src=original_asset.attrs["src"]
+        )
+        original_asset_in_soup.replace_with(updated_asset)
+    return soup_copy.prettify()
--- tests/file_operations/test_file_operations.py
+++ tests/file_operations/test_file_operations.py
@@ -3,10 +3,12 @@ from tempfile import TemporaryDirectory
 
 import pytest
 from page_loader.file_operations import (
-    generate_file_name_from_page_url,
+    generate_file_name_from_src,
+    generate_file_name_prefix_from_page_url,
+    save_assets,
     save_file,
-    store_page_content,
 )
+from tests.paths import file_operations_tests_resources_path
 
 
 @pytest.mark.parametrize(
@@ -14,33 +16,44 @@ from page_loader.file_operations import (
     [
         pytest.param(
             "https://ru.hexlet.io/courses",
-            "ru-hexlet-io-courses.html",
+            "ru-hexlet-io-courses",
             id="page url without file extension",
         ),
         pytest.param(
             "https://ru.hexlet.io/courses?foo=bar",
-            "ru-hexlet-io-courses.html",
+            "ru-hexlet-io-courses",
             id="page url without file extension with query parameters",
         ),
         pytest.param(
             "https://ru.hexlet.io/courses.html",
-            "ru-hexlet-io-courses.html",
+            "ru-hexlet-io-courses",
             id="page url with file extension",
         ),
         pytest.param(
             "https://ru.hexlet.io/courses.xml?foo=bar",
-            "ru-hexlet-io-courses.html",
+            "ru-hexlet-io-courses",
             id="page url with file extension and query parameters",
         ),
     ],
 )
-def test_generate_file_name_from_page_url(page_url, expected_file_name):
+def test_generate_file_name_prefix_from_page_url(page_url, expected_file_name):
     assert (
-        generate_file_name_from_page_url(page_url) == expected_file_name
+        generate_file_name_prefix_from_page_url(page_url) == expected_file_name
     ), "wrong file name was generated"
 
 
-def test_save_file():
+def test_generate_file_name_from_src():
+    file_name_prefix = "foo-bar"
+    src = "/assets/professions/nodejs.png"
+
+    file_name = generate_file_name_from_src(file_name_prefix, src)
+
+    expected_file_name = "foo-bar-assets-professions-nodejs.png"
+
+    assert file_name == expected_file_name
+
+
+def test_save_file_str():
     content = "<h1>foo, bar!</h1>"
     file_name = "foo-bar.html"
     with TemporaryDirectory() as folder:
@@ -53,10 +66,23 @@ def test_save_file():
         assert filepath.read_text() == content, "it should write the given content"
 
 
+def test_save_file_bytes():
+    content = b"<h1>foo, bar!</h1>"
+    file_name = "foo-bar.html"
+    with TemporaryDirectory() as folder:
+        folder_path = Path(folder)
+        filepath = save_file(content, file_name, folder_path)
+
+        assert filepath == folder_path.joinpath(
+            file_name
+        ), "it should return path to the file in folder"
+        assert filepath.read_bytes() == content, "it should write the given content"
+
+
 def test_save_file_raises_if_folder_does_not_exist():
     content = "<h1>foo, bar!</h1>"
     file_name = "foo-bar.html"
-    folder = "/folder/that/does/not/exists"
+    folder = Path("/folder/that/does/not/exists")
 
     with pytest.raises(RuntimeError) as runtime_error:
         save_file(content, file_name, folder)
@@ -64,22 +90,19 @@ def test_save_file_raises_if_folder_does_not_exist():
         assert runtime_error.value == "folder for content saving doesn't exist!"
 
 
-def test_store_page_content():
-    page_url = "https://ru.hexlet.io/courses.html"
-    content = "<h1>foo, bar!</h1>"
-
-    expected_file_name = "ru-hexlet-io-courses.html"
+def test_save_assets():
+    asset_content = file_operations_tests_resources_path(
+        "assets/professions/nodejs.png"
+    ).read_bytes()
 
     with TemporaryDirectory() as folder:
-        folder_path = Path(folder)
+        assets_folder = Path(folder).resolve().joinpath("foo-bar_files")
+        assets_folder.mkdir(parents=True, exist_ok=True)
 
-        filepath = store_page_content(page_url, content, folder_path)
+        asset_path = assets_folder.joinpath("foo-bar-assets-professions-nodejs.png")
+        assets_path_with_content = {asset_path: asset_content}
 
-        expected_file_path = str(folder_path.joinpath(expected_file_name).resolve())
+        save_assets(assets_path_with_content)
 
-        assert (
-            filepath == expected_file_path
-        ), "it should return path to the file in folder"
-        assert (
-            Path(filepath).read_text() == content
-        ), "it should write the given content"
+        assert asset_path.exists()
+        assert asset_path.read_bytes() == asset_content
